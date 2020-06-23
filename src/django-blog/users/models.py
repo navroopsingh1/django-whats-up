e69cf6e7f6880aaf0bd4d6daee6de7de555f0411
paperwarfare3@gmail.com <paperwarfare3@gmail.com>
2020-06-23 19:33:16 -0400

    added pagination

--- src/django-blog/users/models.py
+++ src/django-blog/users/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
 	def save(self):
 		super().save()
 		img = Image.open(self.image.path)
-		if img.height > 300 or img.widthh > 300:
+		if img.height > 300 or img.width > 300:
 			output_size = (300, 300)
 			img.thumbnail(output_size)
 			img.save(self.image.path)
