a4451687cecc69fe700152ea98ca7d0029234014
paperwarfare3@gmail.com <paperwarfare3@gmail.com>
2020-06-22 22:28:00 -0400

    added database and post model to admin page

--- src/django-blog/blog/views.py
+++ src/django-blog/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-# Create your views here.
+from .models import Post
 
-posts = [
-	{
-		'author': 'Navroop Singh',
-		'title': 'Blog Post 1',
-		'content': 'First Post',
-		'date_posted': '2020-05-29'
-	},
-		{
-		'author': 'John Doe',
-		'title': 'Blog Post 2',
-		'content': 'Second Post',
-		'date_posted': '2020-05-30'
-	}
-]
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
